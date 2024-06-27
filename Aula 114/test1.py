69f0cd6aec46a3630474ef591a3db89ecb6428be
m4th3us123 <129892197+m4th3us123@users.noreply.github.com>
2024-06-27 15:45:18 -0300

    Add files via upload

---
+++ Aula 114/test1.py
@@ -0,0 +1,9 @@
+a = 33
+b = 33
+
+if a > b:
+    print("A é maior que B")
+elif a == b:
+    print("A é igual a B")
+elif a < b:
+    print("A é menor que B")
---
+++ Aula 114/test2.py
@@ -0,0 +1,6 @@
+def my_function(fname):
+    print(fname + " Castro")
+
+my_function("Matheus")
+my_function("Lucas")
+my_function("Patricia")
